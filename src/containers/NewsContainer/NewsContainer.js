beab24ddc7f2713127e5031a6b673d0b176da0be
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-10-31 14:27:35 -0600

    push up index

--- src/containers/NewsContainer/NewsContainer.js
+++ src/containers/NewsContainer/NewsContainer.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { chooseCountry } from '../../actions/index';
-console.log('choose', chooseCountry)
 
 export class  NewsContainer extends Component {
     constructor() {
