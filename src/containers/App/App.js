631c65ba883635b9738c91384f5ec6dae095c0f1
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-10-30 22:39:31 -0600

    app now has store

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { getNewsData } from '../../apiCalls';
-import * as actions from '../../actions/index';
+import { 
+  selectNav,
+  saveArticle,
+  unsaveArticle,
+  readArticle,
+  unreadArticle,
+  setLoader,
+  hasError } from '../../actions/index'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { PropTypes } from 'prop-types';
+import { errorReducer } from '../../reducers/errorReducer';
+import { favoriteReducer } from '../../reducers/favoriteReducer';
+import { langReducer } from '../../reducers/langReducer';
+import { loadingReducer } from '../../reducers/loadingReducer';
+import { navReducer } from '../../reducers/navReducer';
+import { readReducer } from '../../reducers/readReducer';
 
 
 class App extends Component {
-  constructor() {
-    super();
-  }
 
    componentDidMount= async () => {
     try {
@@ -16,15 +29,37 @@ class App extends Component {
     } catch(error) {
       return error.message
     }
-
   }
 
   render() {
     return (
-      <p> hi </p>
+      <main>
+
+      </main>
 
     )
   }
 }
 
-export default App;
+export const mapStateToProps = state => ({
+  error: errorReducer,
+  favorites: favoriteReducer,
+  language: langReducer,
+  isLoading: loadingReducer,
+  nav: navReducer,
+  read: readReducer
+})
+
+export const mapDispatchToProps = dispatch => (
+  bindActionCreators({
+    selectNav,
+    saveArticle,
+    unsaveArticle,
+    readArticle,
+    unreadArticle,
+    setLoader,
+    hasError
+  }, dispatch)
+)
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
