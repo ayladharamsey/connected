c1cf28ef23d826e98de364131613286f8145f440
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-10-31 10:24:07 -0600

    create newsContainer with select fields

--- src/containers/NewsContainer/NewsContainer.js
+++ src/containers/NewsContainer/NewsContainer.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 
-export const NewsContainer = () => {
+export const NewsContainer = (countryOptions) => {
+    const options = countryOptions.map(country => {
+        return <option value ={country}>{country}</option>
+    })
+    
     return (
         <section>
+            <select name ="countries">
+                {options}
+            </select>
 
         </section>
     )
