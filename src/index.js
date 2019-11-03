858633583afd0e9d69fcb72312b986ecb40f1eac
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-11-03 14:03:09 -0700

    app setup for local storage

--- src/index.js
+++ src/index.js
@@ -8,8 +8,17 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from '../src/containers/App/App';
 import * as serviceWorker from './serviceWorker';
+import { submitState, storeState } from './localStorage' 
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, submitState(), composeWithDevTools());
+
+store.subscribe(() => {
+    storeState({
+        data: store.getState().newsDataReducer,
+        favorites: store.getState().favoriteReducer,
+        completed: store.getState().readReducer
+    })
+})
 
 ReactDOM.render(
     <Provider store={store}>
