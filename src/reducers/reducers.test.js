2167ac016c298dd0e23088257eff742d3ff48b6b
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-11-03 23:41:30 -0700

    update reducers testing

---
+++ src/reducers/reducers.test.js
@@ -0,0 +1,67 @@
+import { chooseArticleReducer } from './chooseArticleReducer';
+import { countryReducer } from './countryReducer';
+import { errorReducer } from './errorReducer';
+import { favoriteReducer } from './favoriteReducer';
+import { langReducer } from './langReducer';
+import { loadingReducer } from './loadingReducer';
+import { navReducer } from './navReducer';
+import { newsDataReducer } from './newsDataReducer';
+import { readReducer } from './readReducer';
+
+describe('chooseArticleReducer', () => {
+    it('should return the initial state', () => {
+        const expected = {};
+        const result = chooseArticleReducer(undefined, {});
+        expect(result).toEqual(expected);
+      });
+
+      it.skip('should return article if action.type is CHOOSE_ARTICLE', () => {
+        const column = 1;
+        const id = 3;
+        const data = [
+            {
+                author: 'Lemon',
+                title:"Nachfolger John Bercows wird gewählt - derStandard.at",
+                content:"Ich stimme der Verwendung von Cookies für die Zwecke der Webanalyse und digitaler Werbemaßnahmen zu. Auch wenn ich diese Website weiter nutze, gilt dies als Zustimmung. Meine Einwilligung kann ich hier widerrufen.",
+                description:null,
+                url:"https://www.derstandard.de/story/2000110638480/nachfolger-john-bercows-wird-gewaehlt",
+                urlToImage:"https://at.staticfiles.at/img/meta/meta_image_1200x630-4d0796cf00.png",
+                publishedAt:"2019-11-04T05:10:00Z",
+                id:0,
+                countryCode:"at"
+            },
+            {
+                author:null,
+                title:"second example",
+                content:"Words.",
+                description:null,
+                url:"cool.com",
+                urlToImage:"cooler.com",
+                publishedAt:"2019-11-04T05:10:00Z",
+                id:1,
+                countryCode:"at"
+            }
+        ]
+        const mockAction = {
+            type: 'CHOOSE_ARTICLE', 
+            column: column,
+            id: id,
+            data: data
+        };
+        const mockArticle = {
+            author:null,
+            title:"second example",
+            content:"Words.",
+            description:null,
+            url:"cool.com",
+            urlToImage:"cooler.com",
+            publishedAt:"2019-11-04T05:10:00Z",
+            id:1,
+            countryCode:"at"
+        }
+
+        const result = chooseArticleReducer({}, mockAction);
+        expect(result).toEqual(mockArticle); // result is coming back undefined and idk why
+      });
+
+})
