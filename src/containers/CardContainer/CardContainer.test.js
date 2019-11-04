8b947d694f8a99a4a8985e2320dd0caa92bb8776
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-11-04 09:34:00 -0700

    more testing to card container

--- src/containers/CardContainer/CardContainer.test.js
+++ src/containers/CardContainer/CardContainer.test.js
@@ -18,3 +18,22 @@ describe('CardContainer', () => {
         expect(wrapper).toMatchSnapshot();
     })
 })
+
+describe('mapStateToProps', () => {
+    it('should return the nav, favorites, and completed info', () => {
+        let nav, favorites, completed;
+        let mockState = {
+            nav, 
+            favorites,
+            completed
+        }
+        let expected = {
+            nav, 
+            favorites,
+            completed
+        }
+        
+        const props = mapStateToProps(mockState);
+        expect(props).toEqual(expected)
+    })
+})
