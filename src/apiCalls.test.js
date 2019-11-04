37db61a48fa43d5a6abedccbc25a7a10dcedb944
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-11-04 08:01:46 -0700

    update async testing

--- src/apiCalls.test.js
+++ src/apiCalls.test.js
@@ -1,4 +1,4 @@
-import { getNewsData, cleanedNewsData } from './apiCalls'
+import { getNewsData, cleanNewsData } from './apiCalls'
 
 describe('getNewsData', () => {
     const mockResponse = [{}, {}, {}, {}];
@@ -31,4 +31,54 @@ describe('getNewsData', () => {
         });
         expect(getNewsData()).rejects.toEqual(Error('There was an error getting your news data.'));
     });
+});
+
+describe('cleanNewsData', () => {
+    it('should return back a cleaned news article of article data with only the keys we want', () => {
+        const allData = [
+            {
+                author: 'timmy',
+                title: 'i want candy',
+                content: 'you are mean',
+                description: 'testing is hard',
+                source: 'www.cool.com', 
+                url: 'yup',
+                urlToImage: 'www.cool.com',
+                publishedAt: 'march 10',
+                id: 0,
+                countryCode: 'da',
+                logo: 'www.logo.com',
+                details: 'so many details'
+            },
+            {
+                author: 'billy',
+                title: 'i do notwant candy',
+                content: 'you are mean but i like it',
+                description: 'testing is hard but it isnt',
+                source: 'www.uncool.com', 
+                url: 'nope',
+                urlToImage: 'www.uncool.com',
+                publishedAt: 'march 12',
+                id: 1,
+                countryCode: 'po',
+                logo: 'www.logos.com',
+                details: 'so many deets'
+                } 
+        ]
+        const receivedData =
+            {
+            author: 'timmy',
+            title: 'i want candy',
+            content: 'you are mean',
+            description: 'testing is hard',
+            source: 'www.cool.com', 
+            url: 'yup',
+            urlToImage: 'www.cool',
+            publishedAt: 'march 10',
+            id: 0,
+            countryCode: 'da'
+            }
+            
+            expect(getNewsData()).resolves.toEqual(receivedData)
+    })
 })
