e3b2ed8978690032bd87ee545bf2a7c7e7a1602e
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-11-03 22:50:21 -0700

    update NewsContainer with testing

---
+++ src/containers/NewsContainer/NewsContainer.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { NewsContainer, mapDispatchToProps, mapStateToProps } from './NewsContainer'
+import {
+    chooseCountry,
+    setNewsData
+  } from '../../actions/index';
+
+  describe('NewsContainer', () => {
+    let wrapper; 
+    const filterByNav = jest.fn();
+    
+    it('should match the snapshot', () => {
+        wrapper = shallow(<NewsContainer 
+        filterByNav={filterByNav}
+        />)
+    })
+
+  })
