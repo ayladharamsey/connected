962544382887c96e9ba8d0ec5e8d4a5b4c518f78
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-11-04 10:01:41 -0700

    update dispatch tests newscard

--- src/containers/NewsCard/NewsCard.test.js
+++ src/containers/NewsCard/NewsCard.test.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { NewsCard, mapStateToProps, mapDispatchToProps } from './NewsCard';
+import {
+    saveArticle,
+    unsaveArticle,
+    readArticle,
+    unreadArticle,
+    chooseArticle 
+} from '../../actions/index'
 
 describe('NewsCard', () => {
     let wrapper;
@@ -12,6 +19,98 @@ describe('NewsCard', () => {
     it('should match the snapshot', () => {
         expect(wrapper).toMatchSnapshot();
     })
+})
 
+describe('mapStateToProps', () => {
+    it('should return data', () => {
+        let data;
+        let mockState = {
+          data
+        }
     
+        let expected = {
+          data
+        }
+    
+        const props = mapStateToProps(mockState);
+        expect(props).toEqual(expected)
+      })
+})
+
+describe('mapDispatchToProps', () => {
+    let mockDispatchedAction;
+    
+    beforeEach(() => {
+      mockDispatchedAction = jest.fn();
+    })
+
+    it('should call call saveArticle action with dispatch', () => {
+        const article = {
+            title: 'i am cool',
+            content: 'no one is cool',
+            id: 3
+        }
+        const mockAction = saveArticle(article);
+        const props = mapDispatchToProps(mockDispatchedAction);
+    
+        props.saveArticle(article);
+    
+        expect(mockDispatchedAction).toHaveBeenCalledWith(mockAction);
+    })
+
+    it('should call call unsaveArticle action with dispatch', () => {
+        const article = {
+            title: 'i am cool',
+            content: 'no one is cool',
+            id: 3
+        }
+        const mockAction = unsaveArticle(article);
+        const props = mapDispatchToProps(mockDispatchedAction);
+    
+        props.unsaveArticle(article);
+    
+        expect(mockDispatchedAction).toHaveBeenCalledWith(mockAction);
+    })
+
+    it('should call call readArticle action with dispatch', () => {
+        const article = {
+            title: 'i am cool',
+            content: 'no one is cool',
+            id: 3
+        }
+        const mockAction = readArticle(article);
+        const props = mapDispatchToProps(mockDispatchedAction);
+    
+        props.readArticle(article);
+    
+        expect(mockDispatchedAction).toHaveBeenCalledWith(mockAction);
+    })
+    
+    it('should call call unreadArticle action with dispatch', () => {
+        const article = {
+            title: 'i am cool',
+            content: 'no one is cool',
+            id: 3
+        }
+        const mockAction = unreadArticle(article);
+        const props = mapDispatchToProps(mockDispatchedAction);
+    
+        props.unreadArticle(article);
+    
+        expect(mockDispatchedAction).toHaveBeenCalledWith(mockAction);
+    })
+
+    it('should call call chooseArticle action with dispatch', () => {
+        const article = {
+            title: 'i am cool',
+            content: 'no one is cool',
+            id: 3
+        }
+        const mockAction = chooseArticle(article);
+        const props = mapDispatchToProps(mockDispatchedAction);
+    
+        props.chooseArticle(article);
+    
+        expect(mockDispatchedAction).toHaveBeenCalledWith(mockAction);
+    })
 })
